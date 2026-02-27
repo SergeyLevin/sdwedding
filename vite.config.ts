2cd498fc06f2b75a5dcd67b69c3c0c69f9802cdb
SergeyLevin <levinsergeymgn74@gmail.com>
2026-02-27 10:40:47 +0500

    Fix Vite base for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-﻿import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
@@ -7,11 +7,11 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
 
   // GitHub Pages: https://username.github.io/<repo>/
-  // В GitHub Actions передадим:
+  // � GitHub Actions ���������:
   // GH_PAGES="true"
   // REPO_NAME="<repo>"
-  const repo = process.env.REPO_NAME ? String(process.env.REPO_NAME) : '';
-  const isGhPages = process.env.GH_PAGES === 'true';
+  const repo = '';
+  const isGhPages = false;
   const base = (isGhPages && repo) ? ('/' + repo + '/') : '/';
 
   return {
